c85e6a64cecab481aed915fd1ffdc7a2b6d20685
Joel Bruner <joel@brunerd.com>
2020-10-18 18:32:25 -0500

    v0.9.12
    Further work on preventing octals from being used in JSONPath indices, numbers from being used to reference object properties, and strings with leading zeroes from referencing array members (numeric strings without leading zeroes may reference array members, this affords easy inclusion of JSON Pointer)

--- jsonpath.js
+++ jsonpath.js
@@ -1,4 +1,4 @@
-// JSONPath 0.9.10 - XPath for JSON
+// JSONPath 0.9.12 - XPath for JSON
 // Copyright (c) 2020 Joel Bruner (https://github.com/brunerd)
 // Copyright (c) 2020 "jpaquit" (https://github.com/jpaquit)
 // Copyright (c) 2007 Stefan Goessner (goessner.net)
@@ -22,14 +22,14 @@ function jsonPath(obj, expr, arg) {
 		escapeUnicode: arg && arg.escapeUnicode || false,
 		result: [],
 		normalize: function(expr) {
-		
+
 			//work on strings only, pass through all others (like a pre-objectified path array)
 			if (expr.constructor === null || expr.constructor !== String) { return expr }
-			
+
 			var pathStack=[]
 			var baldRecursion=false
 			var lastLastIndex=0;
-			
+
 			//trim any leading/trailing whitespace, reverse the string
 			var revExpr=expr.replace(/^\s*/,"").replace(/\s*$/,"").split('').reverse().join('')
 
@@ -46,7 +46,7 @@ function jsonPath(obj, expr, arg) {
 				if((lastLastIndex+L1Match[0].length) !== Level1Regex.lastIndex || L1Match[0] === "" ){
 					throw new SyntaxError("Malformed path expression: " + expr)
 				}
-		
+
 				//.key1 or ..key2 - ([\w\d$]*[A-Za-z_$])(\.{1,2})
 				//.* or ..* - (\*?)(\.{1,2})
 				if(L1Match[1] || L1Match[3]) {
@@ -61,7 +61,7 @@ function jsonPath(obj, expr, arg) {
 					else if(L1Match[1] === '') { Level1Regex.lastIndex=lastLastIndex;  break }
 				}
 				//(\*?)(\.{1,2}) - just the dots ..
-				else if(L1Match[4]) {							
+				else if(L1Match[4]) {
 					if(L1Match[4] === '.'){ break }
 					else if(!pathStack.length){ baldRecursion=true; break }
 					else if (pathStack[0].expression === "..") {
@@ -79,7 +79,7 @@ function jsonPath(obj, expr, arg) {
 					var openBracket=0, closedBracket=1;
 
 					//set L2Regex to where we are in L1regex
-					Level2Regex.lastIndex = Level1Regex.lastIndex				
+					Level2Regex.lastIndex = Level1Regex.lastIndex
 					do {
 						//get a L2Match match to the exec on revExpr
 						L2Match=Level2Regex.exec(revExpr)
@@ -87,10 +87,10 @@ function jsonPath(obj, expr, arg) {
 						if (L2Match === null || subLastLastIndex === Level2Regex.lastIndex || subLastLastIndex + L2Match[0].length !== Level2Regex.lastIndex ) {
 							throw new SyntaxError("Malformed path expression : " + expr)
 						}
-				
+
 						//reverse things back
 						L2Match = L2Match.map(function(s){return (!s ? s : s.split('').reverse().join(''))})
-				
+
 						//'key' or "key" - (["'])(.*?)\1(?!\\)
 						if(L2Match[2] === ''){ subArray.unshift(L2Match[2]) }
 						else if(L2Match[2]){
@@ -110,10 +110,13 @@ function jsonPath(obj, expr, arg) {
 							if (needsDelimiter && !isSlice) { Level2Regex.lastIndex=subLastLastIndex; break; } else { needsDelimiter=true }
 							if (isSlice && intraSlice) { intraSlice=false }
 
-							//eval in strict to catch octals, regex limits characters here, so eval is safe
-							try {!eval(L2Match[4])} catch(e){throw new Error(e + ": " + L2Match[4])}
-							//subArray.unshift(Number(L2Match[4]))
-							pendingData.unshift(Number(L2Match[4]))
+							//catch octal indices regardless of strict mode
+							if (L2Match[4] !== "0" && (L2Match[4][0] === "0" || (L2Match[4][0] === "-" && L2Match[4][1] === "0"))){
+								throw new Error("Octal indices are disallowed: " + L2Match[4])
+							}
+							else{
+								pendingData.unshift(Number(L2Match[4]))
+							}
 						}
 						//(-) - from JSON Pointer, represents the index AFTER the last one, always non-existent
 						else if(L2Match[5]){
@@ -134,7 +137,7 @@ function jsonPath(obj, expr, arg) {
 								//slice expression (numbers and/or filter expression)
 								subArray.unshift({"expression":pendingData})
 							}
-						
+
 							//reset
 							pendingData=[], needsDelimiter=false
 							if (isSlice) { intraSlice=false; isSlice=false }
@@ -148,10 +151,10 @@ function jsonPath(obj, expr, arg) {
 							if (pendingData.length === 1 && pendingData[0] !== null && (pendingData[0].constructor === String || !(pendingData[0].constructor === Number || pendingData[0].expression[0] === "(" ))){
 								break
 							}
-						
+
 							//if nothing pending or we had previous colon, insert a colon to represent an empty slice slot ::
 							if(!pendingData.length || intraSlice) { pendingData.unshift(null) } 
-						
+
 							//change state
 							if (!intraSlice) { intraSlice=true }
 						}
@@ -165,21 +168,21 @@ function jsonPath(obj, expr, arg) {
 
 							//L3 regex: quoted strings, open parens, closed parens, ruby style regex (L), ruby style regex (R), equals sign, and arbitrary characters
 							var Level3Regex=/(["'])(.*?)\1(?!\\)|(\()|(\))|(\/.*?\/(?!\\)\s*~=)|(~=\s*\/.*?\/(?!\\))|(==?((?:=|!)))|(.)/g
-					
+
 							if (isSlice) { intraSlice = false }
 							else if (needsDelimiter) { break }
-					
+
 							//set our start point to be the same as where we are
 							Level3Regex.lastIndex = Level2Regex.lastIndex
 							do {
 								//keep working on revExpr
 								L3Match = Level3Regex.exec(revExpr)
-						
+
 								//(["'])(.*?)\1(?!\\) - quoted string
 								//escape @ for substitution in P.eval
 								if(L3Match[1]) { 
 									filterText+=L3Match[0].replace(/@/g, "@\\")
-								}				
+								}
 								//(\() - open parens
 								else if(L3Match[3]) {
 									filterText+=L3Match[3]
@@ -195,7 +198,7 @@ function jsonPath(obj, expr, arg) {
 								else if(L3Match[5]||L3Match[6]) { 
 									//escape @ for escaping substitution in P.eval
 									filterText+=L3Match[0].replace(/@/g, "@\\")
-								}								
+								}
 								//(==?((?:=|!))) - normalizes == and != to their strict equality equivalents
 								else if(L3Match[7]) { 
 									//rewrite to === or !== (reversed)
@@ -212,7 +215,7 @@ function jsonPath(obj, expr, arg) {
 
 								if (closeParens === openParens){
 									needsDelimiter=true
-								
+
 									//before we break check if the next char is a question mark and if so include that
 									if(revExpr[Level3Regex.lastIndex] === '?'){
 										if(isSlice){
@@ -222,38 +225,38 @@ function jsonPath(obj, expr, arg) {
 										filterText+="?"
 										Level3Regex.lastIndex = Level3Regex.lastIndex+1
 									}
-								
+
 									//set our Level2Regex index to where we were in this
 									Level2Regex.lastIndex = Level3Regex.lastIndex
 									//reverse back to normal and store this in the array of items
-									var filterTextFinal = filterText.split('').reverse().join('')							
+									var filterTextFinal = filterText.split('').reverse().join('')
 									pendingData.unshift({"expression":filterTextFinal})
-								
+
 									break;
 								}
 							} while (Level3Regex.lastIndex !== 0 && Level3Regex.lastIndex !== revExpr.length)
-						
+
 							if (closeParens !== openParens) { break }
 						}
-						
+
 						// (\[) - open bracket, the end of L2 for now
 						else if(L2Match[9]){
-					
+
 							//empty brackets
 							if(Level2Regex.lastIndex - Level1Regex.lastIndex === 1) {
 								break
 							} 
 							else {
 								Level1Regex.lastIndex = Level2Regex.lastIndex
-								break									
+								break
 							}
 						}
 						//\s* - spaces just advance on...
 
 						//catch if we skip ahead or are stuck next match
-						subLastLastIndex=Level2Regex.lastIndex					
+						subLastLastIndex=Level2Regex.lastIndex
 					} while(Level2Regex.lastIndex !== 0 && Level2Regex.lastIndex !== revExpr.length )
-					
+
 					//if there is a pending number, write it
 					if(pendingData.length === 1 && !isSlice) { subArray.unshift(pendingData[0]) }
 					else if (pendingData.length){
@@ -277,61 +280,69 @@ function jsonPath(obj, expr, arg) {
 				}
 				//to catch later if we skip ahead from a bad match or not...
 				lastLastIndex=Level1Regex.lastIndex
-
 			} while(Level1Regex.lastIndex !== 0 && Level1Regex.lastIndex !== revExpr.length )
-	
+
 			if (!hasRoot || baldRecursion || Level1Regex.lastIndex !== revExpr.length) { throw new SyntaxError("Malformed path expression: " + expr) }
 
-			return pathStack			
+			return pathStack
 		},
-		asPath: function(path) {			
+		asPath: function(path) {
 			if(P.resultType === "PATH_OBJECT"){ return {"path":path} }
-			
+
 			var qt = P.singleQuoteKeys ? "'" : '"';
 			var p=(P.resultType === "PATH_JSONPOINTER" ? "" : "$");
 			var x = path.slice()
-			
+
 			//create p, the path string representation
 			for (var i=0,n=x.length; i<n; i++){
-				
+
 				if(P.resultType === "PATH_JSONPOINTER") {
 					p += "/" + (x[i].constructor === Number ? x[i] : x[i].replace(/~/g,"~0").replace(/\//g,"~1"))
 				}
 				//else JSONPath string
 				else {
-					var pathString = x[i].constructor === Number ? "["+x[i]+"]" : (P.resultType === "PATH_DOTTED" && /^[A-Za-z_$][\w\d$]*$/.test(x[i]) ? "." + x[i] : ("["+ qt + (P.singleQuoteKeys ? x[i].replace(/'/g,"\\'") : x[i].replace(/"/g,'\\"')) + qt + "]").replace(/\n/g,'\\n').replace(/[\b]/g,'\\b').replace(/\f/g,'\\f').replace(/\r/g,'\\r').replace(/\t/g,'\\t').replace(/[\u0000-\u001f\u007f]/g, function(chr) {return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).slice(-4)}));					
+					var pathString = x[i].constructor === Number ? "["+x[i]+"]" : (P.resultType === "PATH_DOTTED" && /^[A-Za-z_$][\w\d$]*$/.test(x[i]) ? "." + x[i] : ("["+ qt + (P.singleQuoteKeys ? x[i].replace(/'/g,"\\'") : x[i].replace(/"/g,'\\"')) + qt + "]").replace(/\n/g,'\\n').replace(/[\b]/g,'\\b').replace(/\f/g,'\\f').replace(/\r/g,'\\r').replace(/\t/g,'\\t').replace(/[\u0000-\u001f\u007f]/g, function(chr) {return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).slice(-4)}));
 
 					//escape Unicode characters \u style
 					if (P.escapeUnicode){
 						pathString = pathString.replace(/[\u007F-\uFFFF]/g, function(chr) { return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).slice(-4) })
-					}				
+					}
 					p += pathString
 				}
 			}
 			return p;
 		},
 		store: function(p, v) {
+			//if we are escaping unicode and a string
+			if (P.escapeUnicode && v !== null && v.constructor === String){
+				v = v.replace(/[\u007F-\uFFFF]/g, function(chr) { return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).slice(-4) })
+			}
 
 			if (p) { P.result[P.result.length] = /^PATH/.test(P.resultType) ? P.asPath(p) : v }
 			return !!p;
 		},
 		trace: function(expr, val, path) {
-			
+
 			if(expr === false) return expr
-			
+
 			//if we have an expression
 			if (expr.length) {
 
-				var x = expr.slice()								
+				var x = expr.slice()
 				var loc = x.shift();
 
-				if (loc === false) { return loc }
-				
+				if(val !== null && Array.isArray(val) && loc.constructor === String && loc.match(/^0/)){
+					throw new Error("Property name '"+ loc +"' is a string with leading zeros and target is an array!")
+				}
+				else if(val !== null && val.constructor === Object && loc.constructor === Number){
+					throw new Error("Property '"+ loc +"' is a number and target is an object!")
+				}
+
 				//if loc is negative and val is an array or string, resolve the negative index
 				if(loc.constructor === Number && Math.sign(loc) === -1 && (val instanceof Array || val.constructor === String)) { 
 					loc = (val.length+loc) 
 				}
-								
+
 				//an array represents a union, it can store: strings (key names), numbers (array indices or numeric property names), and objects (expressions)
 				//example: ["key",0,{"expression":"*"},{"expression":"?(@name =~ /key.*/)"}]
 				if(Array.isArray(loc)){
@@ -342,7 +353,7 @@ function jsonPath(obj, expr, arg) {
 					}
 				}
 				//{"expression":"..."} -  an singular object containing an expression key
-				else if(loc.constructor === Object) {					
+				else if(loc.constructor === Object) {
 					//[0,1,null] - an array in an expression is a slice
 					if(Array.isArray(loc.expression)){
 						P.slice(loc.expression, x, val, path);
@@ -350,7 +361,7 @@ function jsonPath(obj, expr, arg) {
 					//* - star wildcard character
 					else if (loc.expression === "*") {
 						P.walk(loc.expression, x, val, path, function(m,l,x,v,p) { var tx = x.slice(); tx.unshift(m); P.trace(tx,v,p); });
-					}					
+					}
 					//.. recursive descent
 					else if (loc.expression === "..") {
 						P.trace(x, val, path);
@@ -381,7 +392,7 @@ function jsonPath(obj, expr, arg) {
 							} 
 						});
 					}
-				}					
+				}
 				//else we are either a string or a number
 				else if (val && val[loc] !== undefined) {
 					var tpath = path.slice()
@@ -420,13 +431,13 @@ function jsonPath(obj, expr, arg) {
 				val = val.split('')
 				var isString=true
 			} else { var isString=false }
-			
+
 			if (val instanceof Array) {
 				var str="", len, start, end, step=1;
-				
+
 				//if the loc array has any undefined holes set them to null
 				loc[0]=loc[0] !== undefined ? loc[0] : null; loc[1]=loc[1] !== undefined ? loc[1] : null; loc[2]=loc[2] !== undefined ? loc[2] : null
-				
+
 				if ((loc[2] === null || loc[2].constructor === Number ? loc[2] : P.eval(loc[2].expression,val,path[path.length-1])) === 0) { 
 					throw new RangeError("Slice step cannot be zero: [" + loc.join(":") + "]") 
 				}
@@ -447,17 +458,17 @@ function jsonPath(obj, expr, arg) {
 				//adjust the bounds of the default depending on the value and the type of step in cases of negative values
 				start = (start < 0) ? Math.max(Math.sign(step) === -1 ? -1 : 0,start+len) : Math.min(len,start);
 				end = (end < 0) ? Math.max(Math.sign(step) === -1 ? -1 : 0,end+len) : Math.min(len,end);
-				
+
 				//if step is negative go the other way
 				if(Math.sign(step) === -1){ var op=">" } else { var op="<" }
-				
+
 				//use eval to resolve op 
 				for (var i=start; eval(i+op+end); i+=step){
 					//strore arrays proper
 					if(!isString){ 
 						var texpr = expr.slice()
 						texpr.unshift(i)
-						P.trace(texpr, val, path);					
+						P.trace(texpr, val, path);
 					}
 					//collect characters into a string
 					else{
@@ -471,12 +482,12 @@ function jsonPath(obj, expr, arg) {
     		}
 		},
 		eval: function(x, _v, _vname) {
-			
+
 			var tx = x.slice()
-			
+
 			//if this is a () expression, strip the outer parens
 			if ((/^\(.*?\)$/).test(x)) { tx = tx.replace((/^\((.*?)\)$/),"$1") }
-			
+
 			//remove all all data between "" '' and //, split by semi-colon
 			//remove all spaces before ( and collapse multiple spaces down to a single space
 			var forbiddenInvocations=tx.split('').reverse().join('')
@@ -488,14 +499,14 @@ function jsonPath(obj, expr, arg) {
 				.split('').reverse().join('').split(' ')
 				//anything that remains with ( that not at the beginning of a line and has character before it
 				.filter(function(f){return (/\(/).test(f)})
-				.filter(function(f){return (/[!^]\(|[\w\d_$]\(/).test(f)})			
+				.filter(function(f){return (/[!^]\(|[\w\d_$]\(/).test(f)})
 				//only allow three functions tolerated
 				.filter(function(f){return !((/test\(|exec\(|match\(/).test(f))})
-			
+
 			if(forbiddenInvocations.length){ throw new Error("Invocation violation: " + forbiddenInvocations) };
-			
+
 			try {
-				var evalResult = eval(x.replace(/(^|[^\\])@/g, "$1_v")				
+				var evalResult = eval(x.replace(/(^|[^\\])@/g, "$1_v")
 					.replace(/\\@/g, "@")
 					//ruby regex handling from jpaquit
 					//_v substitution on the left side
@@ -511,7 +522,7 @@ function jsonPath(obj, expr, arg) {
 						}
 					)
 				);
-				
+
 				//return false if falsey
 				if(evalResult === undefined || evalResult === null || (evalResult.constructor === Number && Math.sign(evalResult) === -1) ) { 
 					return false 
@@ -545,8 +556,8 @@ function jsonPath(obj, expr, arg) {
 
 	if (expr && obj !== undefined && (P.resultType == "VALUE" || /^PATH/.test(P.resultType))) {
 		//normalize the JSONPath expression and send to trace along with the obj data and the beginning of the -p path output ["$"]
-		P.trace(P.normalize(expr), obj, []);		
-		
+		P.trace(P.normalize(expr), obj, []);
+
 		//return P.result or an empty array
 		return P.result.length ? P.result : [];
 	}
